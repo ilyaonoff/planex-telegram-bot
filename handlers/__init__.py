b92b692770250919403da7cd3b1d88b381082c5d
ilyaonoff <ilyaonof@yandex.ru>
2022-05-01 22:07:03 +0300

    Implement stubs for task handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,3 @@
 from . import info
 from .settings import set_interval, set_subject, choose_subject
+from .task import get_task, receive_answer
---
+++ handlers/task.py
@@ -0,0 +1,41 @@
+from typing import Tuple, Dict
+
+from aiogram.dispatcher import FSMContext
+from keyboards import default_keyboard
+
+from bot import dp
+from states import UserStates
+
+from aiogram import types
+import random
+
+tasks = {
+    '123': {'task': 'Answer to any question', 'answer': '42'},
+    '5': {'task': 'What is the best telegram bot', 'answer': 'PlanExBot'}
+}
+
+
+def random_task() -> Tuple[str, Dict[str, str]]:
+    return random.choice(list(tasks.items()))
+
+
+@dp.message_handler(state=UserStates.wait_for_task)
+async def get_task(message: types.Message, state: FSMContext):
+    # TODO make everything consistent
+    task = random_task()
+    async with state.proxy() as data:
+        data['answer'] = task[1]['answer']
+    await UserStates.wait_for_answer.set()
+    await message.answer(task[1]['task'], reply_markup=default_keyboard)
+
+
+@dp.message_handler(state=UserStates.wait_for_answer)
+async def receive_answer(message: types.Message, state: FSMContext):
+    # TODO make everything consistent
+    async with state.proxy() as data:
+        expected_answer = data['answer']
+    await UserStates.wait_for_task.set()
+    if message.text.lower() == expected_answer:
+        await message.answer('Your answer is correct', reply_markup=default_keyboard)
+    else:
+        await message.answer('You are wrong', reply_markup=default_keyboard)

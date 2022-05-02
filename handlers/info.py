168df116e2af6dbd0e1c11d915e5b61b6f49e43f
ilyaonoff <ilyaonof@yandex.ru>
2022-05-03 00:38:46 +0300

    Remove redundant state

--- handlers/info.py
+++ handlers/info.py
@@ -10,11 +10,11 @@ from states import UserStates
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
     logger.info(f'Start or help message from user {message.from_user.id}')
-    await UserStates.default.set()
+    await UserStates.settings.set()
     await message.answer(messages['start'].format(user=message.from_user), reply_markup=default_keyboard)
 
 
 @dp.message_handler(Text(equals='ℹ️ Информация'),
-                    state=[UserStates.default, UserStates.wait_for_task, UserStates.wait_for_answer])
+                    state=[UserStates.wait_for_task, UserStates.wait_for_answer])
 async def info(message: types.Message):
     await message.answer(messages['info'], reply_markup=default_keyboard)
--- states/user_states.py
+++ states/user_states.py
@@ -2,7 +2,6 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
 class UserStates(StatesGroup):
-    default = State()
     settings = State()
     interval = State()
     subject = State()

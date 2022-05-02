8f39fd28e2e916a871f1df29c7b8ce616473596b
ilyaonoff <ilyaonof@yandex.ru>
2022-05-03 01:44:25 +0300

    Fix the first keyboard after /start

--- handlers/info.py
+++ handlers/info.py
@@ -3,7 +3,7 @@ from aiogram import types
 from bot import dp
 from bot_logging import logger
 from bot import messages
-from keyboards import default_keyboard
+from keyboards import default_keyboard, settings_keyboard
 from states import UserStates
 
 
@@ -11,7 +11,7 @@ from states import UserStates
 async def start_handler(message: types.Message):
     logger.info(f'Start or help message from user {message.from_user.id}')
     await UserStates.settings.set()
-    await message.answer(messages['start'].format(user=message.from_user), reply_markup=default_keyboard)
+    await message.answer(messages['start'].format(user=message.from_user), reply_markup=settings_keyboard)
 
 
 @dp.message_handler(Text(equals='ℹ️ Информация'),
